a3a666770285c0d9e39449d6ffb834cb50c55852
Steve Wilson <sbwilso7@asu.edu>
2018-04-01 13:08:37 -0700

    Initial commit

---
+++ SimonSays/src/StartGame.java
@@ -0,0 +1,13 @@
+
+public class StartGame {
+
+	public static void main(String[] args) {
+		// TODO open loading screen and start menu
+		
+		//begin game loop
+		boolean exit = false;
+		while(exit==false) {
+			
+		}
+	}
+}
