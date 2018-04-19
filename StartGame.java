1dfa299cc6f6040b3b602f5d7898ec8e039b8667
mnowe-asu <38012774+mnowe-asu@users.noreply.github.com>
2018-04-19 16:01:36 -0400

    Add files via upload

---
+++ StartGame.java
@@ -0,0 +1,14 @@
+
+public class StartGame {
+
+	public static void main(String[] args) {
+		// TODO open loading screen and start menu
+		
+		//begin game
+		System.out.println("hi!");
+		System.out.println("Good Luck!");
+		System.out.println("Hello!");
+		
+		
+	}
+}
